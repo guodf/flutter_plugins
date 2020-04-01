f2f2b166c0426af756a1591029371c0df06050e6
guodf <guo_net_win@live.com>
2020-04-02 01:15:15 +0800

    fix

--- install_apk/example/lib/main.dart
+++ install_apk/example/lib/main.dart
@@ -1,12 +1,5 @@
-import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:path/path.dart' as p;
-import 'package:install_apk/install_apk.dart';
-import 'package:local_permissions/local_permissions.dart';
-import 'package:local_permissions/manifest.dart' as manifest;
-import 'package:common_path_provider/common_path_provider.dart';
-import 'package:download_manager/download_manager.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
