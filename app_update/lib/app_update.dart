45d6938c9559e6a6e0e8d675818021ea44819bb6
guodf <guo_net_win@live.com>
2020-04-15 00:35:11 +0800

    update

--- app_update/lib/app_update.dart
+++ app_update/lib/app_update.dart
@@ -1,12 +1,12 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
-import 'dart:math';
-import 'package:dart_utils/date_util.dart';
 import 'package:dart_utils/logger_util.dart';
 import 'package:dart_utils/version_util.dart';
 import 'package:device_info/device_info.dart';
 import 'package:download_manager/download_manager.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/services.dart';
 import 'package:install_apk/install_apk.dart';
@@ -15,61 +15,94 @@ import 'package:package_info/package_info.dart';
 class AppUpdate {
   static const MethodChannel _channel = const MethodChannel('app_update');
 
-  static Future<String> get platformVersion async {
+  Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
-  static Timer _timer;
+  BuildContext context;
+  Timer _timer;
 
-  static bool _updateing = false;
+  bool _updateing = false;
 
-  static Future check(String url) async {
+  Future check(String url) async {
     try {
       if (_updateing) {
-        Logger.info("已存在进行中的升级任务...");
         return;
       }
       _updateing = true;
       final packInfo = await PackageInfo.fromPlatform();
       final oldVersion = packInfo.version;
-      var resp = await http.get("$url/version/check/${packInfo.packageName}");
+      var resp = await http.get("$url/last/version/${packInfo.packageName}");
       if (resp.statusCode == HttpStatus.ok) {
-//      {
-//        "version":"",
-//        "name":"",
-//        "appId":"",
-//        "armeabi-v7a":"",
-//        "arm64-v8a":"",
-//        "x86_64":"",
-//      }
         var map = json.decode(resp.body) as Map<String, dynamic>;
-        if (VersionUtil.compare(map["version"], oldVersion) > 0) {
-          final android = await DeviceInfoPlugin().androidInfo;
-          final cpu = android.supportedAbis.first;
-          final downloadId =
-              await DownloadManager.download(map[cpu], title: map["name"]);
-          // 获取下载状态，并安装
-          _timer = Timer.periodic(Duration(seconds: 2), (timer) {
-            DownloadManager.status(downloadId).then((status) {
-              Logger.info(["download", downloadId, status].toString());
-              if (status == (1 << 3)) {
-                if (!_timer.isActive) {
-                  return;
-                }
-                _timer?.cancel();
-                DownloadManager.filePath(downloadId).then((filePath) {
-                  Logger.info(["install apk", filePath].toString());
-                  if(filePath!=null) {
-                    InstallApk.installApk(filePath).then((value){
-                      Logger.info("安装结果:$value");
-                    });
-                  }
-                  _reset();
-                });
-              }
-            });
-          });
+        if (map["code"] == 10000) {
+          var data = map["data"];
+          if (VersionUtil.compare(data["version"]["version"], oldVersion) > 0) {
+            final android = await DeviceInfoPlugin().androidInfo;
+            final cpu = android.supportedAbis.first?.replaceAll("-", "_");
+
+            var fileUrl = data["files"]
+                ?.firstWhere((item) => item["cpu"] == cpu)["file"] as String;
+
+            if (fileUrl == null || fileUrl.isEmpty) {
+              fileUrl = data["files"]
+                      ?.firstWhere((item) => item["cpu"] == "all")["file"]
+                  as String;
+            }
+            if (fileUrl != null && fileUrl.length > 0) {
+              showDialog(
+                  context: context,
+                  barrierDismissible: false,
+                  builder: (context) {
+                    return SimpleDialog(
+                      title: Text(
+                        "更新提醒",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10))),
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.symmetric(vertical: 10),
+                          alignment: Alignment.center,
+                          child: Text(
+                            "发现新版本是否立即更新?",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            Expanded(
+                              flex: 1,
+                              child: FlatButton(
+                                child: Text("残忍拒绝"),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                              ),
+                            ),
+                            Expanded(
+                              flex: 1,
+                              child: FlatButton(
+                                child: Text("立即更新"),
+                                onPressed: () {
+                                  _update(fileUrl,packInfo.appName);
+                                  Navigator.pop(context);
+                                },
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    );
+                  });
+            }
+            // });
+          }
         }
       }
     } catch (e) {
@@ -78,8 +111,31 @@ class AppUpdate {
     }
   }
 
-  static _reset() {
+  _update(String downUrl,String name) async{
+    final downloadId =
+        await DownloadManager.download(downUrl, title: name);
+    // 获取下载状态，并安装
+    // _timer = Timer.periodic(Duration(seconds: 2), (timer) {
+    DownloadManager.status(downloadId).then((status) {
+      if (status == (1 << 3)) {
+        if (!_timer.isActive) {
+          return;
+        }
+        _timer?.cancel();
+        DownloadManager.filePath(downloadId).then((filePath) {
+          if (filePath != null) {
+            InstallApk.installApk(filePath).then((value) {});
+          }
+          _reset();
+        });
+      }
+    });
+  }
+
+  _reset() {
     _updateing = false;
     _timer = null;
   }
+
+  AppUpdate.of(this.context);
 }
